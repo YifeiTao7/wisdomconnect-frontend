4afd59db63243456f828bcf18cbc339c5bfeb4a2
Yifei Tao <u7272166@anu.edu.au>
2022-08-11 00:31:24 +1000

    1

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  productionSourceMap: false,
   publicPath: './',
-  outputDir: 'dist',
-  assetsDir: 'assets',
+  outputDir: 'test', //打包输出目录
+  assetsDir: './static', //放置生成的静态资源
+  filenameHashing: true, // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
+  lintOnSave: false, //设置是否在开发环境下每次保存代码时都启用 eslint验证
+
+
   devServer: {
-    host:'0.0.0.0',
     port: 8080,
     https:false,
     open:true,
